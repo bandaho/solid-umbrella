f5c0ca010567bed3486a17ce74c33115bdcc5b66
Banda <opilane@HP-CZC6297B9B>
2018-04-10 18:10:10 +0300

    Lisan kohalikust kasutajast uue rea

--- TerePimedus/TerePimedus/Program.cs
+++ TerePimedus/TerePimedus/Program.cs
@@ -10,8 +10,9 @@ namespace TerePimedus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Darkness, my old friend");
-            Console.WriteLine("I've come to talk with you again");
+            Console.WriteLine("Hello Darkness, my old friend"); // esimene lause siit koolikeskkonnast
+            Console.WriteLine("I've come to talk with you again"); // lisatud githubist
+            Console.WriteLine("Because a vision softly creeping"); // lisatud kohalikust kasutajast
         }
     }
 }
